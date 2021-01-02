b6086114c65d0c6160d0871e5157777dd0f3716d
claireporter26 <58444269+claireporter26@users.noreply.github.com>
2021-01-02 16:46:45 +0000

    first commit

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+// const navToggle = document.querySelector('.nav-toggler');
+// const navMain = document.querySelector('#navbarSupportedContent')
+
+// navToggle.addEventListener('click', () => {
+//     navMain.classList.toggle('nav--visible');
+// })
