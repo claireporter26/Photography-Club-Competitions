d2efc9fcb2a54784a69b2a06355495f9a7300154
claireporter26 <58444269+claireporter26@users.noreply.github.com>
2021-01-16 22:45:12 +0000

    added modal functionality

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,25 @@
 // navToggle.addEventListener('click', () => {
 //     navMain.classList.toggle('nav--visible');
 // })
+
+var exampleModal = document.getElementById('imgZoomModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  
+  var chosenImage = event.relatedTarget.src;
+//   console.log(chosenImage)
+  // Extract info from data-bs-* attributes
+//   var largeImage = chosenImage.getAttribute('data-bs-whatever');
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  
+//   var imageArea = exampleModal.querySelector(".modal-image")
+  var imageArea = exampleModal.querySelector(".modal-image")
+
+  imageArea.src = chosenImage;
+ 
+  
+  
+})
